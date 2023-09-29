9fd810fff57f6ce84311db178bf99c7c9ce06ba3
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-09-29 19:24:38 +0800

    get user in admin

--- projects/client/src/components/admin/branch/UserTransaction.jsx
+++ projects/client/src/components/admin/branch/UserTransaction.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Box, Select, Text } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const UserTransaction = () => {
-  return (
-    <div>UserTransaction</div>
-  )
-}
+  const [user, setUser] = useState([]);
+  const getUser = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/api/admin/branch/transaction`
+      );
+      setUser(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default UserTransaction
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <Box fontFamily={"montserrat"} ml={"48px"} mt={"24px"}>
+      <Text fontSize={"32px"}>User Order</Text>
+      {user.length > 0 ? (
+        <Select w={"400px"} placeholder="Select Customer">
+          {user.map((users) => (
+            <option key={users.id} value={users.id}>
+              {users.username}
+            </option>
+          ))}
+        </Select>
+      ) : (
+        <Text>No users available</Text>
+      )}
+    </Box>
+  );
+};
+
+export default UserTransaction;
